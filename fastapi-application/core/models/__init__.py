c1ffaaa55e66da67ce88eba11b776e4535629d26
Rustam <sypooit@gmail.com>
2024-12-26 13:34:28 +0300

    delete users model

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,11 +1,9 @@
 __all__ = (
     "db_helper",
     "Base",
-    "User",
     "Project"
 )
 
 from .db_helper import db_helper
 from .base import Base
-from .user import User
 from .projects import Project
