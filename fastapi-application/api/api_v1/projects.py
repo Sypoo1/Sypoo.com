02f063751d2f68639cf001ffad30f4ed3212f523
Rustam <sypooit@gmail.com>
2024-12-26 14:16:48 +0300

    add delte and update project crud

--- fastapi-application/api/api_v1/projects.py
+++ fastapi-application/api/api_v1/projects.py
@@ -11,6 +11,7 @@ from core.models import db_helper
 from core.schemas.project import (
     ProjectRead,
     ProjectCreate,
+    ProjectUpdate,
 )
 from crud import project as project_crud
 
@@ -39,5 +40,37 @@ async def create_project(
     project = await project_crud.create_project(
         session=session,
         project_create=project_create,
+    )
+    return project
+
+
+@router.put("/{project_id}", response_model=ProjectRead)
+async def update_project(
+    project_id: int,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    project_update: ProjectUpdate,
+):
+    project = await project_crud.update_project(
+        session=session,
+        project_id=project_id,
+        project_update=project_update,
+    )
+    return project
+
+
+@router.delete("/{project_id}", response_model=ProjectRead)
+async def delete_project(
+    project_id: int,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+):
+    project = await project_crud.delete_project(
+        session=session,
+        project_id=project_id,
     )
     return project
--- fastapi-application/crud/project.py
+++ fastapi-application/crud/project.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 
 from core.models import Project
-from core.schemas.project import ProjectCreate
+from core.schemas.project import ProjectCreate, ProjectUpdate
 
 
 async def get_all_projects(
@@ -23,5 +23,41 @@ async def create_project(
     project = Project(**project_create.model_dump())
     session.add(project)
     await session.commit()
-    # await session.refresh(project)
+    await session.refresh(project)
+    return project
+
+
+async def update_project(
+    session: AsyncSession,
+    project_id: int,
+    project_update: ProjectUpdate,
+) -> Project:
+    stmt = select(Project).where(Project.id == project_id)
+    result = await session.execute(stmt)
+    project = result.scalar_one_or_none()
+
+    if project is None:
+        raise ValueError(f"Project with id:{project_id} not found")
+
+    for key, value in project_update.model_dump().items():
+        setattr(project, key, value)
+
+    await session.commit()
+    await session.refresh(project)
+    return project
+
+
+async def delete_project(
+    session: AsyncSession,
+    project_id: int,
+) -> Project:
+    stmt = select(Project).where(Project.id == project_id)
+    result = await session.execute(stmt)
+    project = result.scalar_one_or_none()
+
+    if project is None:
+        raise ValueError(f"Project with id:{project_id} not found")
+
+    await session.delete(project)
+    await session.commit()
     return project
